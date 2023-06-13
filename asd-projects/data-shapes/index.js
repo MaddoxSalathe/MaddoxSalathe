20ac16816b93c948dce23d3bbdc0944c49202a4c
MaddoxSalathe <maddox.salathe@gmail.com>
2023-06-13 16:36:41 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -61,11 +61,26 @@ for (let i = 0; i < dataShapes.length; i++) {
   }
 }
   // TODO 3-a: add a function that handles the static display type
+function handleStatic(data) {
+  setBackgroundWithObject(data);
+  animationDetails.displayType = 1;
+}
+
+// ...
+
+
 
   // TODO 4-a: add a function that handles the good display type
-
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  }
   // TODO 5-a: add a function that handles the bad display type
-
+  function handleBad(data, repeat) {
+    repeat += 1;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3;
+  }
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
   /////////////////////////////////////////////////
@@ -73,15 +88,18 @@ for (let i = 0; i < dataShapes.length; i++) {
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
   }
-
+handleStatic(dataShapes[currentIndex]);
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
   }
-
+  let currentShape = dataShapes[currentIndex];
+  handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   function badDisplay() {
     // TODO 5-b: call your handleBad function
   }
-
+  let currentShape = dataShapes[currentIndex];
+  let repeat = currentShape.repeat;
+  handleBad(currentShape, repeat);
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO ABOVE HERE ////////
   /////////////////////////////////////////////////
