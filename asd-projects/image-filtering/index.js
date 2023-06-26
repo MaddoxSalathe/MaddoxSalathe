ebab91ab9464ec8d81fb96cd52b21e79cbb4ac94
MaddoxSalathe <maddox.salathe@gmail.com>
2023-06-26 16:06:49 +0000

    saving image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -21,18 +21,40 @@ function resetAndRender() {
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
 
-  
+  applyFilter(reddify);
 
   // do not change the below line of code
   render($("#display"), image);
 }
+
 
 /////////////////////////////////////////////////////////
 // "apply" and "filter" functions should go below here //
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(filterFunction) {
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      
+      // Step 2a: Retrieve the RGB string value from the image array
+      var rgbString = image[i][j];
 
+      // Step 2b: Convert the RGB string to an array of RGB numbers
+      var rgbNumbers = rgbStringToArray(rgbString);
+      
+      // Step 2c: Alter the contents of the RGB numbers array
+
+      //rgbNumbers[GREEN] = 255;
+      filterFunction(rgbNumbers);
+      // Step 2d: Convert the RGB numbers array back to a string
+      var newRgbString = rgbArrayToString(rgbNumbers);
+
+      // Step 2e: Assign the new RGB string back into the image array
+      image[i][j] = newRgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
 
@@ -41,6 +63,12 @@ function applyAndRender() {
 
 
 // TODO 3: Create reddify function
+function reddify(array) {
+  array[RED] = 200;
+  //$("#apply").on("click", function () {
+  //applyFilter(reddify);
+};
+  render($("#display"), image);
 
 
 // TODO 6: Create more filter functions
